408c4b819941c3f1ed5567dfd1fc067c941fef3e
Be-konjouyu <167198715+Be-konjouyu@users.noreply.github.com>
2024-11-13 16:28:29 +0900

    ProBuilderを導入

--- Assets/Scripts/Enemy/EnemyAttack_ShockWave.cs
+++ Assets/Scripts/Enemy/EnemyAttack_ShockWave.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class EnemyAttack_ShockWave : MonoBehaviour
 {
+    EnemyBase enemy;    // 敵の共通する値、処理を継承したスクリプトを保持する
+
+    //衝撃波関係-------------------------------------------------------------------
+    int         damage;                 // ダメージ量
+    float       expandSpeed = 5f;       // 拡大スピード
+    float       maxRadius = 10f;        // 最大半径
+    int         arcPointsCount = 12;    // 円周上のポイント数
+    GameObject  damagePointPrefab;      // ダメージ判定用プレハブ
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //敵の共通する値、処理を継承したスクリプトを取得する
+        enemy = GetComponent<EnemyBase>();
+
+        //値の設定
+        if(enemy)
+        {
+            damage = enemy.attackPower;
+        }
     }
 
     // Update is called once per frame
