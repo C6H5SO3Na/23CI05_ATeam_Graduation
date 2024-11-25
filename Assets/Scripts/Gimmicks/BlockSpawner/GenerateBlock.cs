ed70ee50eb8da9a4d380cc3e084e8d7754be4806
Be-konjouyu <167198715+Be-konjouyu@users.noreply.github.com>
2024-11-25 23:47:16 +0900

    ブロックを生成する処理を仮実装
    現在はブロックが一つ生成されたら、生成されたブロックが削除されるまで新しくブロックが生成されないように設定している。
    後に生成する位置毎にすでに生成されているかを判断し、新しく生成するかを決めるようにする。

--- Assets/Scripts/Gimmicks/BlockSpawner/GenerateBlock.cs
+++ Assets/Scripts/Gimmicks/BlockSpawner/GenerateBlock.cs
@@ -2,19 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenerateBlock : MonoBehaviour
+public class GenerateBlock : MonoBehaviour, IStartedOperation
 {
+    [SerializeField]
+    private GameObject generatingBlockPrefab;                           // 生成するブロック
+    private List<GameObject> generatedBlocks = new List<GameObject>();  // 生成したブロックのリスト
+    private int maxObjects;                                             // 生成できるブロックの数
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        maxObjects = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    //ブロックが無ければブロックを生成する
+    public void StartedOperation()
     {
-        
+        if(generatingBlockPrefab)
+        {
+            //生成したブロックが消滅していたらリストから削除しておく
+            generatedBlocks.RemoveAll(block => block == null);
+
+            //ブロックが生成されていなかったら生成する
+            if(generatedBlocks.Count < maxObjects)
+            {
+                //生成位置を決める
+                Vector3 position = new Vector3(9, 1, 9);
+
+                //ブロックの生成
+                GameObject newBlock = Instantiate(generatingBlockPrefab, position, Quaternion.identity);
+
+                //生成したブロックをリストに登録
+                generatedBlocks.Add(newBlock);
+            }
+            else
+            {
+                Debug.LogWarning("既に生成されている");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("生成するオブジェクトが設定されていない");
+        }
     }
 }
