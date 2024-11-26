7078dddbaaa6360ad8cd8082940585b40de327fd
Be-konjouyu <167198715+Be-konjouyu@users.noreply.github.com>
2024-11-26 13:22:43 +0900

    ブロックが敵にぶつかったときのみ消滅するように変更

--- Assets/Scripts/Gimmicks/Break.cs
+++ Assets/Scripts/Gimmicks/Break.cs
@@ -7,7 +7,8 @@ public class Break : MonoBehaviour
     //プレイヤー、もしくは敵にぶつかった場合壊れる
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+        //if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+        if(collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(this.gameObject);
         }
