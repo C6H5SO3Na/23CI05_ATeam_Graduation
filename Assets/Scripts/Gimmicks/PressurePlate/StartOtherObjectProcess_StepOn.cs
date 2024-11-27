1cb4af4b506ec6c046de13b3a0e44ff547230cca
Be-konjouyu <167198715+Be-konjouyu@users.noreply.github.com>
2024-11-27 10:17:27 +0900

    感圧板が一度押されたら反応しなくなるようにした

--- Assets/Scripts/Gimmicks/PressurePlate/StartOtherObjectProcess_StepOn.cs
+++ Assets/Scripts/Gimmicks/PressurePlate/StartOtherObjectProcess_StepOn.cs
@@ -7,27 +7,36 @@ public class StartOtherObjectProcess_StepOn : MonoBehaviour
     [SerializeField]
     private GameObject targetObject;    // 処理を行わせるオブジェクト
 
+    private bool isPressed = false;
+
     private void OnTriggerStay(Collider other)
     {
-        if(!other.CompareTag("Enemy"))
+        //一度も押されていなかったら処理を行う
+        if(!isPressed)
         {
-            if (targetObject)
+            if (!other.CompareTag("Enemy"))
             {
-                //targetObjectが起動される動作を実装しているか確認する
-                IStartedOperation objectHavingStartedOperation = targetObject.GetComponent<IStartedOperation>();
-                if (objectHavingStartedOperation != null)
+                if (targetObject)
                 {
-                    //実装している処理をさせる
-                    objectHavingStartedOperation.StartedOperation();
+                    //targetObjectが起動される動作を実装しているか確認する
+                    IStartedOperation objectHavingStartedOperation = targetObject.GetComponent<IStartedOperation>();
+                    if (objectHavingStartedOperation != null)
+                    {
+                        //実装している処理をさせる
+                        objectHavingStartedOperation.StartedOperation();
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"{targetObject.name}は起動される処理が実装されていません");
+                    }
                 }
                 else
                 {
-                    Debug.LogWarning($"{targetObject.name}は起動される処理が実装されていません");
+                    Debug.LogWarning("処理を行わせるオブジェクトが指定されていません");
                 }
-            }
-            else
-            {
-                Debug.LogWarning("処理を行わせるオブジェクトが指定されていません");
+
+                //押されたことを記憶する
+                isPressed = true;
             }
         }
     }
