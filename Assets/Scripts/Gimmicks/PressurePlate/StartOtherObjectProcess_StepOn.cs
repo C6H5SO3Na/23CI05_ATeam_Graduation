dbbcde83eec0d54ef7db4407983c3ff4f25ee102
Be-konjouyu <167198715+Be-konjouyu@users.noreply.github.com>
2024-12-18 12:08:06 +0900

    透明化するブロック仮実装

--- Assets/Scripts/Gimmicks/PressurePlate/StartOtherObjectProcess_StepOn.cs
+++ Assets/Scripts/Gimmicks/PressurePlate/StartOtherObjectProcess_StepOn.cs
@@ -5,40 +5,90 @@ using UnityEngine;
 public class StartOtherObjectProcess_StepOn : MonoBehaviour
 {
     [SerializeField]
-    private GameObject targetObject;    // 処理を行わせるオブジェクト
+    GameObject targetObject;    // 処理を行わせるオブジェクト
 
-    private bool isPressed = false;     // 押されたかを記憶する
+    bool isOnce = false;        // 一度だけしか押せないか(処理しないか)決める
+    bool isPressed;             // 押されたかを記憶する
 
-    private void OnTriggerStay(Collider other)
+    void Start()
     {
-        //一度も押されていなかったら処理を行う
+        //値の初期化
+        isPressed = false;
+    }
+
+    //感圧板を押したとき
+    void OnTriggerEnter(Collider other)
+    {
         if(!isPressed)
         {
-            if (!other.CompareTag("Enemy"))
+            //ボックスコライダーのみに反応する
+            if (other is BoxCollider)
             {
-                if (targetObject)
+                //プレイヤーか投擲物に反応する
+                if (other.CompareTag("Player") || other.CompareTag("ThrowingObject"))
                 {
-                    //targetObjectが起動される動作を実装しているか確認する
-                    IStartedOperation objectHavingStartedOperation = targetObject.GetComponent<IStartedOperation>();
-                    if (objectHavingStartedOperation != null)
+                    if (targetObject)
                     {
-                        //実装している処理をさせる
-                        if(objectHavingStartedOperation.StartedOperation())
+                        //targetObjectが起動される動作を実装しているか確認する
+                        IStartedOperation objectHavingStartedOperation = targetObject.GetComponent<IStartedOperation>();
+                        if (objectHavingStartedOperation != null)
                         {
-                            //処理が行われたら、押されたことを記憶する
-                            isPressed = true;
+                            //実装している「感圧板を押したとき」の処理をさせる
+                            objectHavingStartedOperation.ProcessWhenPressed();
+                            
+                            //一度しか押せない場合
+                            if (isOnce)
+                            {
+                                //処理が行われたら、押されたことを記憶する
+                                isPressed = true;
+                            }
+                        }
+                        else
+                        {
+                            Debug.LogWarning($"{targetObject.name}は起動される処理が実装されていません");
                         }
                     }
-                    else
-                    {
-                        Debug.LogWarning($"{targetObject.name}は起動される処理が実装されていません");
-                    }
-                }
-                else
-                {
-                    Debug.LogWarning("処理を行わせるオブジェクトが指定されていません");
                 }
             }
         }
+    }
+
+    //感圧板から離れたとき
+    void OnTriggerExit(Collider other)
+    {
+        if (!isPressed)
+        {
+            //ボックスコライダーのみに反応する
+            if (other is BoxCollider)
+            {
+                //プレイヤーか投擲物に反応する
+                if (other.CompareTag("Player") || other.CompareTag("ThrowingObject"))
+                {
+                    if (targetObject)
+                    {
+                        //targetObjectが起動される動作を実装しているか確認する
+                        IStartedOperation objectHavingStartedOperation = targetObject.GetComponent<IStartedOperation>();
+                        if (objectHavingStartedOperation != null)
+                        {
+                            //実装している「感圧板から離れたとき」の処理をさせる
+                            objectHavingStartedOperation.ProcessWhenStopped();
+                        }
+                        else
+                        {
+                            Debug.LogWarning($"{targetObject.name}は起動される処理が実装されていません");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 一度しか押せないかどうか決める関数
+    /// </summary>
+    /// <param name="isOnceValue"> 一度しか押せない場合true、何度でも押せる場合false </param>
+    public void SetIsOnce(bool isOnceValue)
+    {
+        isOnce = isOnceValue;
     }
 }
