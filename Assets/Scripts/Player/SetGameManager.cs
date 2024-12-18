70a7ef5c60a3e064e5d062ea89f2e990d4a29889
宮川 龍希 <23ci0538@jec.ac.jp>
2024-12-18 14:25:23 +0900

    プレイヤーがダメージを受けたときに点滅する機能実装完了

--- Assets/Scripts/Player/SetGameManager.cs
+++ Assets/Scripts/Player/SetGameManager.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class SetGameManager : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         GetComponent<PlayerController>().SetInstance(gameManager);
     }
