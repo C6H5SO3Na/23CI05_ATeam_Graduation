3d9196ded331a35943fd9f55f826bcce4b4b4048
宮川 龍希 <23ci0538@jec.ac.jp>
2024-12-25 10:37:47 +0900

    選択枠を点滅するようにした タイトルの誤字修正

---
+++ Assets/Scripts/Common/Blinking.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blinking : MonoBehaviour
+{
+    Image image;
+    Color tmpColor;
+    int sign;
+    int Sign //ïÑçÜ
+    {
+        set { sign = Mathf.Clamp(value, -1, 1); }
+        get { return sign; }
+    }
+    [SerializeField] float blinkSpeed;
+    void Start()
+    {
+        image = GetComponent<Image>();
+        Sign = -1;
+    }
+
+    void Update()
+    {
+        tmpColor = image.color;
+        if (!IsWithinRangeExclusive(tmpColor.a, 0f, 1f))
+        {
+            ChangeSign();
+        }
+        tmpColor.a += blinkSpeed * Time.deltaTime * Sign;
+        image.color = tmpColor;
+    }
+
+    void ChangeSign()
+    {
+        Sign = -Sign;
+    }
+
+    bool IsWithinRangeExclusive(float value, float min, float max)
+    {
+        if (min > max)//ì¸ÇÍë÷Ç¶
+        {
+            (max, min) = (min, max);
+        }
+        return min <= value && max >= value;
+    }
+}
