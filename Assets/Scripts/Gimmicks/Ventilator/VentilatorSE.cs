307e8009fba40b7a3000a2d93e6ac5e319808ea0
宮川 龍希 <23ci0538@jec.ac.jp>
2025-02-04 00:29:36 +0900

    風のエフェクトとSEを実装

---
+++ Assets/Scripts/Gimmicks/Ventilator/VentilatorSE.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentilatorSE : MonoBehaviour
+{
+    public AudioClip[] ventilatorSE = new AudioClip[3];
+}
